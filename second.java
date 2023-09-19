741877f10e04452c4dc15f5082f9c15271d4073a
krishna-raz <krishnaraj143007@gmail.com>
2023-09-19 08:31:42 +0530

    add modtification

--- second.java
+++ second.java
@@ -1,14 +1,16 @@
 package java;
 class second {
     int a = 10;
+   static int f = 10;
     double b = 21.7;
 
     public static void main(String[] args) {
 
         int c = 12;
-
+        System.out.println(f); //use of static
+        
         second obj = new second();
-        System.out.println(obj.a);
+        System.out.println(obj.a);//use of object
         System.out.println(obj.b);
         System.out.println(c);
         System.out.println(obj.a + " " + obj.b + " " + c);
