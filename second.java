ff42bef9f0c916a0f3b7faeefbb305c70c62faae
krishna-raz <krishnaraj143007@gmail.com>
2023-09-19 08:00:35 +0530

    add day 1 java program 1

---
+++ second.java
@@ -0,0 +1,18 @@
+package java;
+class second {
+    int a = 10;
+    double b = 21.7;
+
+    public static void main(String[] args) {
+
+        int c = 12;
+
+        second obj = new second();
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+        System.out.println(c);
+        System.out.println(obj.a + " " + obj.b + " " + c);
+
+    }
+
+}
